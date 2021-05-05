e7fd02772d1e4909ef4037fda9f651a8bcb1c215
Bruno Arieira <bruno.arieira@atlanse.pt>
2021-05-05 14:20:41 +0100

    Criacao do projeto

---
+++ src/main/java/com/barieira/asMinhasFinancas/AsMinhasFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.barieira.asMinhasFinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsMinhasFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsMinhasFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barieira/asMinhasFinancas/AsMinhasFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barieira.asMinhasFinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsMinhasFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
